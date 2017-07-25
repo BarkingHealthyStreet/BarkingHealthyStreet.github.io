2bf6d2cbe7153961b5b8c131c29b02197a196d1d
BarkingHealthyStreet <30418768+BarkingHealthyStreet@users.noreply.github.com>
2017-07-25 17:50:17 +0100

    Add files via upload

--- js/contact_me.js
+++ js/contact_me.js
@@ -20,7 +20,7 @@ $(function() {
                 firstName = name.split(' ').slice(0, -1).join(' ');
             }
             $.ajax({
-                url: "././mail/contact_me.php",
+                url: "//formspree.io/liliana.ortega.g@gmail.com",
                 type: "POST",
                 data: {
                     name: name,
